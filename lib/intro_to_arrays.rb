26d4864ab478a33cc30733ed41d8e7d25ebd77b8
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-18 17:01:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,9 +1,9 @@
-def instantiate_new_array(array)
+def instantiate_new_array
   array = []
   return array
 end
 
-def array_with_two_elements(coin)
+def array_with_two_elements
   coin = ["heads", "tails"]
   return coin
 end
